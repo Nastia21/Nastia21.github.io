96e479f983466cb40a19ee93536245798d442bc0
Nastia21 <74502108+Nastia21@users.noreply.github.com>
2021-12-24 03:20:52 +0200

    Add files via upload

---
+++ examples/1/js/script.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+	let divParent = document.createElement('div');
+	divParent.className = "divParent";
+
+    let div = document.createElement('div');
+    div.className = "headline";
+    div.innerHTML = "Ask magic ball right now!";
+	divParent.appendChild(div);
+	
+    let divTxt = document.createElement('div');
+    divTxt.className = "txtQuestion";
+    divTxt.innerHTML = 'What do you want to ask? Enter your question and then click on the ball';
+    divParent.appendChild(divTxt);
+
+	//Введення питання
+	var input = document.createElement("input");
+    input.type = "text";
+	input.placeholder = "You can ask everything you want";
+    input.className = "inputTxt"; 
+	divParent.appendChild(input);
+	
+	//Шар
+    let divShar = document.createElement("div");
+    divShar.className = "shar";
+    
+    var buttonShake = document.createElement("button");
+    buttonShake.className = "shakeTxt";
+    buttonShake.innerHTML = "Shake it!";
+    
+    divShar.appendChild(buttonShake);
+	divParent.appendChild(divShar);
+	
+	var arrayAnswer = ["Yes", "No", "You need to think more", "Perhaps", "Worth trying", "Surely"];
+    var sizeOfArray =  arrayAnswer.length;
+    
+	buttonShake.addEventListener('click', changeTxt);
+	function changeTxt(){
+		if(input.value === "") {
+			alert( "Введіть ваше питання");
+			}
+			else{
+				var index = Math.random()*( (sizeOfArray-1) +  0) -0;  
+				setTimeout(function() {
+				buttonShake.innerHTML = " ";
+				buttonShake.innerHTML = arrayAnswer[index.toFixed()];
+				},2000)
+				}
+	}
+
+	document.body.append(divParent);
+};
---
+++ examples/4/js/script2.js
@@ -0,0 +1,77 @@
+alert("Введіть ваше ім'я");
+var name = prompt();
+if (name == "")
+{
+	alert("Enter nick!");
+	var name = prompt();
+}
+
+$(document).ready(function(){
+	var numOfWord = 0;
+	var plus = 0;
+	var minus = 0;
+	iteration = 0;
+	$("#user").text( name.toString());
+
+
+		var arrayWord = ["Childhood", "Depth", "District", "Knowledge", "Profit", "Surgeon", "To suspect", "Liberty", "Destination", "Confidence"];
+	var arrayAnswer = ["Дитинство", "Глибина", "Район", "Знання", "Прибуток", "Хірург", "Підозрювати", "Свобода", "Призначення", "Впевненість"];
+	var sizeWord =  arrayWord.length;
+	var sizeAnswer =  arrayAnswer.length;
+	$(".numWord").text( `${numOfWord} / 10`);
+	$("#plus").text( ` Правильних відповідей: ${plus}`);
+	$("#minus").text( ` Неправильних відповідей: ${minus} `);
+	
+		
+	var indGlobal = 0;
+	function appearWord(){
+		var index = Math.floor(Math.random() * sizeWord) + 0; //для пользователя
+		indGlobal = index;
+		$('#word').text(arrayWord[index]);
+	}
+
+	appearWord();
+	
+	$('.check').click(function() {
+		if (iteration < 10)
+		{
+			var res = $('#value').val();
+			var index2 = arrayAnswer.indexOf(res);
+			//alert (index);
+			//alert(res);
+			if (Number(indGlobal) == Number(index2)){
+				plus++;
+				 $("#plus").text( ` Правильних відповідей: ${plus}`);
+				} 
+				else{
+					minus++;
+					$("#minus").text( ` Неправильних відповідей: ${minus} `);
+					}  
+	 $("#value").text(" ");
+	 numOfWord++;
+	 $(".numWord").text( `${numOfWord} / 10`);
+	 appearWord();
+	 iteration++;   
+	}
+	if(iteration == 10)
+	{
+		setTimeout(function () {
+			
+		if(plus<5)
+		{
+			alert("Вам потрібно підтягнути знання!"); 	
+		}
+		if(plus>5 && plus <8)
+		{
+			alert("У вас добра база словарного запасу!"); 	
+		}
+		if(plus>8)
+		{
+			alert("Молодець! Широкий словарний запас!"); 	
+		}
+		},1000);
+		
+	}
+	});
+})
+
